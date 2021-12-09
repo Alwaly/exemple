cfd018bf19c4a142f074e8e8f88a3d6390d43b6b
AAND <ndiayealwaly999@gmail.com>
2021-12-09 19:33:40 +0000

    class ayant une methode qui calcule la n vector

--- vector.py
+++ vector.py
@@ -1,4 +1,9 @@
+from _typeshed import Self
+import math
 class Vecteur:
     def __init__(self,x,y):
         self.x=x
         self.y=y
+    def norme():
+        n=math.sqrt(Self.x**2+Self.y**2)
+        return n
